18ae05de3492c2e94bce56524dc8bce05052b44c
Zachary Mays <89159643+eccoghosp@users.noreply.github.com>
2022-04-28 12:52:19 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='landFinder',
+    version='0.1',
+    packages=['requests', 'bs4', 'pandas', 'sqlalchemy', 're', 'tqdm', 'numpy', 'datetime', 'time', 'random'],
+    url='https://github.com/eccoghosp/Landfinder',
+    license='MIT License',
+    author='Zach Mays',
+    author_email='zachary.a.mays@gmail.com',
+    description='A we scraper to gather properties from landwatch.com and save it to a SQL server'
+)
